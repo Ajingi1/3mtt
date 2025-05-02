b8c0438e6f366258c7d305950e32114a59380686
Ahmad <148390443+Ajingi1@users.noreply.github.com>
2025-05-02 20:22:16 +0300

    Counter App
    Counter app created for assessment of 3MTT

---
+++ counter/src/App.js
@@ -0,0 +1,14 @@
+import  './App.css'
+import Counter from './Counter'
+
+function App() {
+  return (
+    <div className="App">
+      <div className="content">
+        <Counter />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ counter/src/Counter.js
@@ -0,0 +1,40 @@
+import { useState, } from 'react'
+import './Counter.css'
+
+
+const Counter = () => {
+    const [count, setCount] = useState(0);
+
+    const handleIncreament = () => {
+        setCount(prev => prev + 1);
+    }
+    const handleDecreament = () => {
+        setCount(prev => {
+            if (prev <= 0) {
+              alert("Counter cannot go below 0");
+              return 0;
+            }
+            return prev - 1;
+          });
+    }
+    const handleReset = () => {
+        setCount(0)
+    }
+    return (
+        <div className="home">
+            <h1>Counter</h1>
+            <div className="counterBox">
+                <input type="number" className="counterDisplay" disabled value={count}/>
+                <div className="buttons">
+                    <button className="btn" onClick={handleIncreament}>Increase</button>
+                    <button className="btn" onClick={handleDecreament}>Decrease</button>
+                </div>
+                <div className="controlBtns">
+                    <button className="reset" onClick={handleReset}>Reset</button>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Counter;
